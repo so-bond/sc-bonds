e9700a78e55d1c21a0687341c3c56e2e1f44bf3c
youssef maghzaz <youssefmaghzaz@gmail.com>
2023-09-18 00:42:02 +0100

    pending testing

--- test/register-status-lifecycle.spec.ts
+++ test/register-status-lifecycle.spec.ts
@@ -1,12 +1,12 @@
 describe("Register (Bond Issuance)", function () {
   describe("status lifecyle", function () {
-    it("should be draft after deploy", async () => {});
-    it("should still be Draft after mint as mint does not affect lifecyle (not process related)", async () => {});
-    it("should be ready after makeReady", async () => {});
-    it("should be revert to Draft after makeReady", async () => {});
-    it("should be Issued after issuance approval", async () => {});
+    it.skip("should be draft after deploy", async () => {});
+    it.skip("should still be Draft after mint as mint does not affect lifecyle (not process related)", async () => {});
+    it.skip("should be ready after makeReady", async () => {});
+    it.skip("should be revert to Draft after makeReady", async () => {});
+    it.skip("should be Issued after issuance approval", async () => {});
   });
   describe("bond reparation", () => {
-    it("It should create a new bond, force some balances then force its issuance", async () => {});
+    it.skip("It should create a new bond, force some balances then force its issuance", async () => {});
   });
 });
